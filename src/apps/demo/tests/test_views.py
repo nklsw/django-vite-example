73a4d18310f3a8ab56dd7d8fae3acef28177c4e5
nklsw <niklas.wahl@mail.de>
2025-04-11 15:13:12 +0200

    fixed vite issue in test env

--- src/apps/demo/tests/test_views.py
+++ src/apps/demo/tests/test_views.py
@@ -5,4 +5,4 @@ from django.test import Client
 def test_demo_index_view(client: Client) -> None:
     response = client.get("/")
     assert response.status_code == HttpResponse.status_code
-    assert b"<h1>Hello World!</h1>" in response.content
+    assert b"<p>...section content goes here...</p>" in response.content
